fd526ed61deac2afd32f0d260652611ccd677558
hyelie <hyelie@postech.ac.kr>
2023-09-02 04:38:30 +0900

    Remove: Remove a refactoring exmaple project It was moved to Naver2Tistory

--- spring/src/main/java/com/playground/spring/extract_subclass_example/before/registration/RegistrationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.playground.spring.extract_subclass_example.before.registration;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegistrationRepository extends JpaRepository<Registration, Integer>{
-    
-}
